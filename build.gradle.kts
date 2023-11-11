c1c57ac19d61432d378166bb103ff5c3e4d98f7e
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2023-11-11 18:57:52 +0100

    build: release 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    `maven-publish`
 }
 
 group = "nl.sanderdijkhuis"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -25,4 +26,37 @@ java {
 
 kotlin {
     jvmToolchain(8)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "hashtocurve-kotlin"
+            from(components["java"])
+            pom {
+                name.set("Hashing to Elliptic Curves for Kotlin")
+                description.set("Implementation of RFC 9380 algorithms for encoding or hashing an arbitrary " +
+                        "byte array to a point on an elliptic curve.")
+                url.set("https://sanderdijkhuis.nl/2023/hashtocurve-kotlin/")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://sanderdijkhuis.nl/2023/hashtocurve-kotlin/blob/main/LICENSE.md")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/sander/hashtocurve-kotlin.git")
+                    developerConnection.set("scm:git:ssh://github.com/sander/hashtocurve-kotlin.git")
+                    url.set("https://github.com/sander/hashtocurve-kotlin")
+                }
+                developers {
+                    developer {
+                        id.set("sander")
+                        name.set("Sander Dijkhuis")
+                        email.set("mail@sanderdijkhuis.nl")
+                    }
+                }
+            }
+        }
+    }
 }
